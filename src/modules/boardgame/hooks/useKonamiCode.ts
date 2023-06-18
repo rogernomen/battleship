3b41fadac6b4ce7a240a27abdc54421ed859d77a
rogernomenginer <roger.nomen@intive.com>
2023-06-18 14:37:53 +0200

    Use Callback

--- src/modules/boardgame/hooks/useKonamiCode.ts
+++ src/modules/boardgame/hooks/useKonamiCode.ts
@@ -1,30 +1,30 @@
-import {useEffect, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 
 const pattern = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
 
 export const useKonamiCode = (callbackFn: () => void) => {
     const [current, setCurrent] = useState(0);
 
+    const keyHandler = useCallback ((event: KeyboardEvent) => {
+        setCurrent((prevState) => {
+            if (event.key !== pattern[prevState]) {
+                return 0;
+            }
+
+            const updatedState = prevState + 1;
+            if (pattern.length === updatedState) {
+                callbackFn();
+                return 0;
+            }
+            return updatedState;
+        });
+    }, [callbackFn]);
+
     useEffect(() => {
-        const keyHandler = (event: KeyboardEvent) => {
-            setCurrent((prevState) => {
-                if (event.key !== pattern[prevState]) {
-                    return 0;
-                }
-
-                const updatedState = prevState + 1;
-                if (pattern.length === updatedState) {
-                    callbackFn();
-                    return 0;
-                }
-                return updatedState;
-            });
-        };
-
         document.addEventListener('keydown', keyHandler, false);
 
         return () => {
             document.removeEventListener('keydown', keyHandler);
         };
-    }, [setCurrent]);
+    }, [keyHandler]);
 }
